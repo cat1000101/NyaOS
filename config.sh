859d1830d46ae21d9edc354a2976657a09367b51
cat1000101 <udbe3333@gmail.com>
2024-07-05 23:23:34 +0300

    tring to learn the stuction of the recommanded osdev struction

---
+++ config.sh
@@ -0,0 +1,22 @@
+SYSTEM_HEADER_PROJECTS="libc kernel"
+PROJECTS="libc kernel"
+
+export MAKE=make
+export HOST=${HOST:-$(./default-host.sh)}
+
+export AR="zig ar -target x86-freestanding"
+export AS=nasm
+export CC="zig cc -target x86-freestanding"
+
+export PREFIX=/usr
+export EXEC_PREFIX=$PREFIX
+export BOOTDIR=/boot
+export LIBDIR=$EXEC_PREFIX/lib
+export INCLUDEDIR=$PREFIX/include
+
+export CFLAGS='-O2 -g'
+export CPPFLAGS=''
+
+# Configure the cross-compiler to use the desired system root.
+export SYSROOT="$(pwd)/sysroot"
+export CC="$CC --sysroot=$SYSROOT -I$SYSROOT$INCLUDEDIR"
---
+++ osdev.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+crosspkgs = pkgs.pkgsCross.i686-embedded;
+in
+pkgs.mkShell {
+    nativeBuildInputs = [ pkgs.pkg-config ];
+    buildInputs = with pkgs; [
+        zig
+        binutils
+        libgcc
+        nasm
+        gnumake
+        clang
+        # crosspkgs.gcc
+    ];
+}
