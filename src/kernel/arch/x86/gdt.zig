397fa637d67af893b6a56629b7357e2e423c69db
cat1000101 <udbe3333@gmail.com>
2024-07-10 19:11:35 +0300

    added the task state manager or in other words tss

--- src/kernel/arch/x86/gdt.zig
+++ src/kernel/arch/x86/gdt.zig
@@ -1,13 +1,54 @@
+const std = @import("std");
 const virtio = @import("virtio.zig");
 
 pub const gdt_entry_struct = packed struct {
     limit_low: u16,
-    base_low: u16,
-    base_middle: u8,
+    base_low: u24,
     access: access_struct,
     limit_high: u4,
     flags: flags_struct,
     base_high: u8,
+};
+
+pub const Tss = packed struct {
+    link: u16,
+    Reserved1: u16,
+    esp0: u32,
+    ss0: u16,
+    Reserved2: u16,
+    esp1: u32,
+    ss1: u16,
+    Reserved3: u16,
+    esp2: u32,
+    ss2: u16,
+    Reserved4: u16,
+    cr3: u32,
+    EIP: u32,
+    EFLAGS: u32,
+    EAX: u32,
+    ECX: u32,
+    EDX: u32,
+    EBX: u32,
+    ESP: u32,
+    EBP: u32,
+    ESI: u32,
+    EDI: u32,
+    es: u16,
+    preserved6: u16,
+    cs: u16,
+    preserved7: u16,
+    ss: u16,
+    preserved8: u16,
+    ds: u16,
+    preserved9: u16,
+    fs: u16,
+    preserved10: u16,
+    gs: u16,
+    preserved11: u16,
+    ldtr: u16,
+    preserved12: u32,
+    iopb: u16,
+    ssp: u32,
 };
 
 pub const gdt_ptr_struct = packed struct {
@@ -31,18 +72,18 @@ pub const access_struct = packed struct {
     dpl: u2, //DPL: Descriptor privilege level field. Contains the CPU Privilege level of the segment. 0 = highest privilege (kernel), 3 = lowest privilege (user applications).
     p: u1, //P: Present bit. Allows an entry to refer to a valid segment. Must be set (1) for any valid segment.
 };
+const NUMBER_OF_ENTRIES: u16 = 0x06;
 
-var gdt_enties: [5]gdt_entry_struct = undefined;
+var gdt_entries: [NUMBER_OF_ENTRIES]gdt_entry_struct = undefined;
+var tss_entry: Tss = std.mem.zeroes(Tss);
 var gdt_ptr: gdt_ptr_struct = undefined;
 
 pub fn initGdt() void {
-    gdt_ptr.limit = (@sizeOf(gdt_entry_struct) * 5) - 1;
-    gdt_ptr.base = @intFromPtr(&gdt_enties);
+    gdt_ptr.limit = (@sizeOf(gdt_entry_struct) * NUMBER_OF_ENTRIES) - 1;
+    gdt_ptr.base = @intFromPtr(&gdt_entries);
+    setTssTable(&tss_entry, 0x10, @sizeOf(Tss));
 
     setGdtGate(
-        0,
-        0,
-        0,
         0,
         0,
         0,
@@ -52,57 +93,58 @@ pub fn initGdt() void {
     setGdtGate(
         1,
         0,
-        0,
-        0,
-        0xFFFF,
-        0xF,
+        0xFFFFF,
         .{ .p = 1, .dpl = 0, .s = 1, .e = 1, .dc = 0, .rw = 1, .a = 0 },
         .{ .g = 1, .db = 1, .l = 0, .preserved = 0 },
     ); //kernel code segment
     setGdtGate(
         2,
         0,
-        0,
-        0,
-        0xFFFF,
-        0xF,
+        0xFFFFF,
         .{ .p = 1, .dpl = 0, .s = 1, .e = 0, .dc = 0, .rw = 1, .a = 0 },
         .{ .g = 1, .db = 1, .l = 0, .preserved = 0 },
     ); //kernel data segment
     setGdtGate(
         3,
         0,
-        0,
-        0,
-        0xFFFF,
-        0xF,
+        0xFFFFF,
         .{ .p = 1, .dpl = 3, .s = 1, .e = 1, .dc = 0, .rw = 1, .a = 0 },
         .{ .g = 1, .db = 1, .l = 0, .preserved = 0 },
     ); // user code segment
     setGdtGate(
         4,
         0,
-        0,
-        0,
-        0xFFFF,
-        0xF,
+        0xFFFFF,
         .{ .p = 1, .dpl = 3, .s = 1, .e = 0, .dc = 0, .rw = 1, .a = 0 },
         .{ .g = 1, .db = 1, .l = 0, .preserved = 0 },
     ); // user data segment
+    setGdtGate(
+        5,
+        @intFromPtr(&tss_entry),
+        @sizeOf(Tss),
+        .{ .p = 1, .dpl = 0, .s = 0, .e = 1, .dc = 0, .rw = 0, .a = 1 },
+        .{ .g = 1, .db = 0, .l = 0, .preserved = 0 },
+    ); // Task State Segment
 
     gdt_flush(&gdt_ptr);
 
     virtio.outb("initialize gdt\n");
+
+    load_tss();
+    virtio.outb("initialize Tss\n");
 }
-fn setGdtGate(num: u32, base_low: u16, base_middle: u8, base_high: u8, limit_low: u16, limit_high: u4, access: access_struct, flags: flags_struct) void {
-    gdt_enties[num].base_low = base_low;
-    gdt_enties[num].base_middle = base_middle;
-    gdt_enties[num].base_high = base_high;
-    gdt_enties[num].limit_low = limit_low;
-    gdt_enties[num].limit_high = limit_high;
-    gdt_enties[num].limit_high = limit_high;
-    gdt_enties[num].flags = flags;
-    gdt_enties[num].access = access;
+fn setGdtGate(num: u32, base: u32, limit: u20, access: access_struct, flags: flags_struct) void {
+    gdt_entries[num].base_low = @truncate(base);
+    gdt_entries[num].base_high = @truncate(base >> 24);
+    gdt_entries[num].limit_low = @truncate(limit);
+    gdt_entries[num].limit_high = @truncate(limit >> 16);
+    gdt_entries[num].flags = flags;
+    gdt_entries[num].access = access;
+}
+
+fn setTssTable(tss: *Tss, ss0: u16, iopb: u16) void {
+    tss.ss0 = ss0; // autofix
+    tss.iopb = iopb; // autofix
 }
 
 fn gdt_flush(gdt_ptr_: *gdt_ptr_struct) void {
@@ -125,4 +167,10 @@ fn gdt_flush(gdt_ptr_: *gdt_ptr_struct) void {
         \\ljmp $0x08, $1f
         \\1:
     );
+}
+
+fn load_tss() void {
+    // Load the Tss into the CPU
+    asm volatile ("movw $0x28 , %%ax");
+    asm volatile ("ltr %%ax");
 }
--- src/kernel/kmain.zig
+++ src/kernel/kmain.zig
@@ -1,7 +1,7 @@
 const tty = @import("tty.zig");
-const virtio = @import("virtio.zig");
-const utils = @import("utils.zig");
-const gdt = @import("gdt.zig");
+const virtio = @import("arch/x86/virtio.zig");
+const utils = @import("arch/x86/utils.zig");
+const gdt = @import("arch/x86/gdt.zig");
 
 export fn kmain() void {
     tty.initialize();
