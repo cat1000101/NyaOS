3e74b0f8f55957c4642a0d1e36ec816d1ff7f03b
Tamir Hadash <udbe3333@gmail.com>
2024-09-05 12:33:58 +0300

    added easier support for talking to ports idk ; - ;

---
+++ src/kernel/arch/x86/port.zig
@@ -0,0 +1,113 @@
+// OUT - Send a byte to the specified I/O port
+pub inline fn outb(port: u16, data: u8) void {
+    asm volatile ("outb %[data], %[port]"
+        :
+        : [data] "a" (data),
+          [port] "Nd" (port),
+    );
+}
+
+// OUT - Send a word to the specified I/O port
+pub inline fn outw(port: u16, data: u16) void {
+    asm volatile ("outw %[data], %[port]"
+        :
+        : [data] "a" (data),
+          [port] "Nd" (port),
+    );
+}
+
+// OUT - Send a double word to the specified I/O port
+pub inline fn outl(port: u16, data: u32) void {
+    asm volatile ("outl %[data], %[port]"
+        :
+        : [data] "a" (data),
+          [port] "Nd" (port),
+    );
+}
+
+// IN - Read a byte from the specified I/O port
+pub inline fn inb(port: u16) u8 {
+    var ret: u8 = 0;
+    asm volatile ("inb %[port], %[ret]"
+        : [ret] "=a" (ret),
+        : [port] "Nd" (port),
+    );
+    return ret;
+}
+
+// IN - Read a word from the specified I/O port
+pub inline fn inw(port: u16) u16 {
+    var ret: u16 = 0;
+    asm volatile ("inw %[port], %[ret]"
+        : [ret] "=a" (ret),
+        : [port] "Nd" (port),
+    );
+    return ret;
+}
+
+// IN - Read a double word from the specified I/O port
+pub inline fn inl(port: u16) u32 {
+    var ret: u32 = 0;
+    asm volatile ("inl %[port], %[ret]"
+        : [ret] "=a" (ret),
+        : [port] "Nd" (port),
+    );
+    return ret;
+}
+
+// INS - Input string (byte) from port into memory
+pub inline fn insb(port: u16, addr: *void, count: u32) void {
+    asm volatile ("rep insb"
+        : [addr] "+D" (addr),
+          [count] "+c" (count),
+        : [port] "d" (port),
+        : "memory"
+    );
+}
+
+// INS - Input string (word) from port into memory
+pub inline fn insw(port: u16, addr: *void, count: u32) void {
+    asm volatile ("rep insw"
+        : [addr] "+D" (addr),
+          [count] "+c" (count),
+        : [port] "d" (port),
+        : "memory"
+    );
+}
+
+// INS - Input string (double word) from port into memory
+pub inline fn insd(port: u16, addr: *void, count: u32) void {
+    asm volatile ("rep insd"
+        : [addr] "+D" (addr),
+          [count] "+c" (count),
+        : [port] "d" (port),
+        : "memory"
+    );
+}
+
+// OUTS - Output string (byte) from memory to port
+pub inline fn outsb(port: u16, addr: *const void, count: u32) void {
+    asm volatile ("rep outsb"
+        : [addr] "+S" (addr),
+          [count] "+c" (count),
+        : [port] "d" (port),
+    );
+}
+
+// OUTS - Output string (word) from memory to port
+pub inline fn outsw(port: u16, addr: *const void, count: u32) void {
+    asm volatile ("rep outsw"
+        : [addr] "+S" (addr),
+          [count] "+c" (count),
+        : [port] "d" (port),
+    );
+}
+
+// OUTS - Output string (double word) from memory to port
+pub inline fn outsd(port: u16, addr: *const void, count: u32) void {
+    asm volatile ("rep outsd"
+        : [addr] "+S" (addr),
+          [count] "+c" (count),
+        : [port] "d" (port),
+    );
+}
