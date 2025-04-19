d6524e684280fd6c9afb090ea92ecd7f9ac7c98a
Tamir Hadash <udbe3333@gmail.com>
2025-04-19 18:56:25 +0300

    fixed the newPageTable function initilizing it with random data smh

--- kernel/src/arch/x86/paging.zig
+++ kernel/src/arch/x86/paging.zig
@@ -231,6 +231,9 @@ pub fn newPageTable(vaddr: u32) memory.AllocatorError!*PageTable {
         .read_write = 1,
         .user_supervisor = if (vaddr < memory.KERNEL_ADDRESS_SPACE) 1 else 0,
     });
+    for (0..1024) |i| {
+        pageTable.setEntery(i, 0, .{});
+    }
     return pageTable;
 }
 
